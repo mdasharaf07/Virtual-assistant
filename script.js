52e3b865750e2bff0441a62cf87db089bd3bbc15
Mohamed Asharaf <mdasharafs2005@gmail.com>
2024-12-29 11:02:09 +0530

    Add files via upload
    Jarvis is an intelligent virtual assistant designed to simplify your daily tasks. Powered by advanced AI and natural language processing, Jarvis can help with scheduling, reminders, information retrieval, and much more. Fully customizable and open-source, it’s the perfect assistant for personal and professional use.
    
    Key Features:
    
    -Voice and text-based interaction
    -Task automation and scheduling
    -Integration with third-party APIs (e.g., weather, news, email)
    -Modular and extensible architecture

---
+++ script.js
@@ -0,0 +1,96 @@
+let btn=document.querySelector("#btn")
+let content=document.querySelector("#content")
+let voice=document.querySelector("#voice")
+
+function speak(text){
+    let text_speak=new SpeechSynthesisUtterance(text)
+    text_speak.rate=1
+    text_speak.pitch=1
+    text_speak.volume=1
+    text_speak.lang="hi-GB"
+    window.speechSynthesis.speak(text_speak)
+}
+
+function wishMe(){
+    let day=new Date()
+    let hours=day.getHours()
+    if(hours>=0 && hours<12){
+        speak("Good Morning Sir")
+    }
+    else if(hours>=12 && hours <16){
+        speak("Good afternoon Sir")
+    }else{
+        speak("Good Evening Sir")
+    }
+}
+// window.addEventListener('load',()=>{
+//     wishMe()
+// })
+let speechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition 
+let recognition =new speechRecognition()
+recognition.onresult=(event)=>{
+    let currentIndex=event.resultIndex
+    let transcript=event.results[currentIndex][0].transcript
+    content.innerText=transcript
+   takeCommand(transcript.toLowerCase())
+}
+
+btn.addEventListener("click",()=>{
+    recognition.start()
+    voice.style.display="block"
+    btn.style.display="none"
+})
+function takeCommand(message){
+   voice.style.display="none"
+    btn.style.display="flex"
+    if(message.includes("hello")||message.includes("hey")){
+        speak("hello sir,what can i help you for you today!?")
+    }
+    else if(message.includes("who are you")||message.includes("what is your name")){
+        speak("i am virtual assistant ,created by Asharaf Sir")
+    }else if(message.includes("open youtube")){
+        speak("opening youtube...")
+        window.open("https://youtube.com/","_blank")
+    }
+    else if(message.includes("open google")){
+        speak("opening google...")
+        window.open("https://google.com/","_blank")
+    }
+    else if(message.includes("open facebook")){
+        speak("opening facebook...")
+        window.open("https://facebook.com/","_blank")
+    }
+    else if(message.includes("goodbye")){
+        speak("goodbye sir anything you want means call me sir")
+    }
+    else if(message.includes("good morning")){
+        speak("goodmorning have a great day sir")
+    }
+    else if(message.includes("good afternoon")){
+        speak("goodafternoon sir have you ate lunch sir")
+    }
+    else if(message.includes("good night")){
+        speak("goodnight sir love you 3000 times")
+    }
+    else if(message.includes("open calculator")){
+        speak("opening calculator..")
+        window.open("calculator://")
+    }
+    else if(message.includes("open whatsapp")){
+        speak("opening whatsapp..")
+        window.open("whatsapp://")
+    }
+    else if(message.includes("time")){
+      let time=new Date().toLocaleString(undefined,{hour:"numeric",minute:"numeric"})
+      speak(time)
+    }
+    else if(message.includes("date")){
+        let date=new Date().toLocaleString(undefined,{day:"numeric",month:"short"})
+        speak(date)
+      }
+    else{
+        let finalText="this is what i found on internet regarding" + message.replace("jarvis","") || message.replace("jarvis","")
+        speak(finalText)
+        window.open(`https://www.google.com/search?q=${message.replace("jarvis","")}`,"_blank")
+    }
+}
